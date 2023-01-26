9389ec183a1ddd7b6186e08f468aa99b5a4133cb
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2023-01-25 21:59:17 -0800

    Still working on it

--- client/src/components/Gundams/GundamCard.js
+++ client/src/components/Gundams/GundamCard.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const GundamCard = () => {
+const GundamCard = ({ gundam }) => {
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      
+    <div
+      style={{
+        display: 'flex',
+        width: '90%',
+        margin: 'auto',
+        gap: '1rem',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+      }}
+    >
+      <div
+        key={gundam.id}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          border: 'black solid',
+          width: '100%',
+          maxWidth: '200px',
+          }}
+        >
+          {gundam.name}
+        <img src={gundam.img_url} alt='gundam_pic' />
+        <br />
+        <button onClick={() => navigate(`/gundams/${gundam.id}`)}>
+            Gundam Details
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default GundamCard;
