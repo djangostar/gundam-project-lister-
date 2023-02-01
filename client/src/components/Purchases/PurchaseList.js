5789eedfbd26cd2001bf327d54ecbc6da9cc1dd8
dylandupasquier <dylandupasquier@gmail.com>
2023-02-01 06:10:37 -0800

    fixed

--- client/src/components/Purchases/PurchaseList.js
+++ client/src/components/Purchases/PurchaseList.js
@@ -1,27 +1,20 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { UserContext } from '../../context/user';
 import PurchaseCard from '../Purchases/PurchaseCard';
 
 const PurchaseList = () => {
   const { purchases } = useContext(UserContext);
- // console.log(typeof purchases);
-
- useEffect(() => {
-console.log(purchases)
- }, [purchases])
 
   return (
     <div>
       <br />
-      {purchases && purchases.map((purchase) => 
-      (
-            <PurchaseCard
-              key={purchase.id}
-              purchase={purchase}
-              gundam={purchase.gundam}
-            />
-         ))
-     }
+      {purchases.map((purchase) => (
+        <PurchaseCard
+          key={purchase.id}
+          purchase={purchase}
+          gundam={purchase.gundam}
+        />
+      ))}
     </div>
   );
 };
